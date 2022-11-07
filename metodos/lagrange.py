fb1c42e6b1f0df94527ba04c4d485ed167a31aa5
Your Name <luis.martinez33@protonmail.com>
2022-11-07 16:12:47 -0600

    Lagrange agregado

--- metodos/lagrange.py
+++ metodos/lagrange.py
@@ -1,12 +1,29 @@
 class Lagrange:
 
-    def __init__(self):
-        self.a = 34
-
     @staticmethod
     def formula():
-        return "g(x) = Σ yi π "
+        return "/Lagrange.png"
 
     @staticmethod
     def methodName():
         return "Lagrange.png"
+
+    def solve():
+        roundValue = 9
+        x = np.array([0, 1, 4, 6])
+        y = np.array([2, 3, 18, 38])
+
+        n = 4
+        xp = 2
+        yp = 0
+        for i in range(n):
+
+            p = 1
+
+            for j in range(n):
+                if i != j:
+                    p = p * (xp - x[j])/(x[i] - x[j])
+
+            yp = yp + p * y[i]
+
+        return yp
