4bd406dbc211cd440cedb4db79d8e7a47aa0b8de
Angel A. Castillo <88498845+AACE1909234@users.noreply.github.com>
2022-11-10 01:44:10 -0600

    Update __init__.py

--- metodos/__init__.py
+++ metodos/__init__.py
@@ -4,7 +4,10 @@ from metodos.interpolacion_lineal import Interpolacion_lineal
 
 from metodos.newton_hacia_atras import Newton_hacia_atras
 from metodos.lagrange import Lagrange
+
 from metodos.punto_fijo import PuntoFijo
+from metodos.metodo_bisectriz import Bisectriz
+from metodos.raphson import Newton_Raphson
 
 from metodos.integracion_trapezoidal import Integracion_trapezoidal
 from metodos.integracion_simson13 import Integracion_simpson13
