2e4901c3b471ed145ab638c2f0ededa9ed415380
Angel A. Castillo <aaer.tablet@gmail.com>
2022-11-24 00:45:38 -0600

    jacobi

---
+++ metodos/jacobi.py
@@ -0,0 +1,63 @@
+from random import randint
+import numpy as np
+
+class Jacobi:
+    @staticmethod
+    def formula():
+        return "/jacobi.png"
+
+    @staticmethod
+    def methodName():
+        return "Jacobi"
+
+    def generatePossibleSolutions(self):
+        solution, val = self.solve()
+        # change this value if you want customized solutions
+        # (optional) if your solution is an integer number, change this value to an integer
+        standard_deviation = 0.5
+        fake_solutions = 4
+
+        rng = np.random.default_rng()
+        for r in range(len(val)):
+            s = rng.normal(val[r], standard_deviation, fake_solutions)
+            r = r + 1
+        s = np.append(s, val)
+        rng.shuffle(s)
+        return s
+
+    def __init__(self):
+        selectProblem = randint(1, 2)
+        if selectProblem == 1:
+            self.A = np.array([[3,-0.1,-0.2],
+                               [0.1,7,-0.3],
+                               [0.3,-0.2,10]])
+            self.B = np.array([[7.85]
+                               [-19.3]
+                               [71.4]])
+            self.problemImage = "jacobi1.png"
+        elif selectProblem == 2:
+           self.A = np.array([[1,-3,5],
+                               [8,-1,-1],
+                               [-2,4,1]])
+           self.B = np.array([[5]
+                               [8]
+                               [4]])
+           self.problemImage = "GaussSeidel2.png"
+        self.x0 = 0 
+        self.x1 = 0
+        self.tol = 0.001
+
+    def solve(self):
+                self.x0 = np.zeros(4)
+                self.x1 = np.zeros(4)
+                while abs(self.x1 - self.x0) < self.tol : 
+                    self.x0 = self.x1  
+                    self.x1[0] = (self.B[0] - self.A[0,1] * self.x0[1] - self.A[0,2] * self.x0[2] - self.A[0,3] * self.x0[3]) / self.A[0,0]
+                    self.x1[1] = (self.B[1] - self.A[1,0] * self.x0[0] - self.A[1,2] * self.x0[2] - self.A[1,3] * self.x0[3]) / self.A[1,1]
+                    self.x1[2] = (self.B[2] - self.A[2,0] * self.x0[0] - self.A[2,1] * self.x0[1] - self.A[2,3] * self.x0[3]) / self.A[2,2]
+                    self.x1[3] = (self.B[3] - self.A[3,0] * self.x0[0] - self.A[3,1] * self.x0[1] - self.A[3,2] * self.x0[2]) / self.A[3,3]                    
+                    k = k + 1
+                error = abs(self.x1 - self.x0)
+                x = self.x1 #numeros de la ecuacion
+                return error
+                
