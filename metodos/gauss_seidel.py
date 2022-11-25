53f857be8619bcef17dadce0e47cd03411c2246a
Angel A. Castillo <aaer.tablet@gmail.com>
2022-11-25 15:05:55 -0600

    update

--- metodos/gauss_seidel.py
+++ metodos/gauss_seidel.py
@@ -26,69 +26,114 @@ class GaussSeidel:
         rng.shuffle(s)
         return s
 
+    def generatePossibleSolutions(self, standard_deviation=0.5):
+            a,b,c = self.solve()
+            # change this value if you want customized solutions
+            # (optional) if your solution is an integer number, change this value to an integer
+            fake_solutions = 2
+
+            rng = np.random.default_rng()
+            s = rng.normal(a, standard_deviation, size=(fake_solutions,3))
+            s = np.append(s,[[a,b,c]], axis=0)
+            rng.shuffle(s)
+            return s
+
     def __init__(self):
+        
         selectProblem = randint(1, 2)
         if selectProblem == 1:
             self.A = np.array([[3, -0.1, -0.2],
-                               [0.1, 7, -0.3],
-                               [0.3, -0.2, 10]])
+                            [0.1, 7, -0.3],
+                            [0.3, -0.2, 10]])
             self.B = np.array([[7.85],
-                               [-19.3],
-                               [71.4]])
-            self.problemImage = "GaussSeidel1.png"
+                            [-19.3],
+                            [71.4]])
+            self.problemImage = "jacobi1.png"
+            self.op = 1
         elif selectProblem == 2:
-            self.A = np.array([[1, -3, 5],
-                               [8, -1, -1],
-                               [-2, 4, 1]])
-            self.B = np.array([[5],
-                               [8],
-                               [4]])
-            self.problemImage = "GaussSeidel2.png"
-
-        self.X0 = np.array([0, 0, 0])
+            self.A = np.array([[3, -0.1, -0.2],
+                            [0.1, 7, -0.3],
+                            [0.3, -0.2, 10]])
+            self.B = np.array([[7.85],
+                            [-19.3],
+                            [71.4]])
+            self.problemImage = "jacobi2.png"
+            self.op = 2
+        
+        self.a = 0
+        self.b = 0
+        self.c = 0
+        self.a1 = 0
+        self.b1 = 0
+        self.c1 = 0
         self.tol = 0.001
-        self.itermax = 100
-        self.tamano = 0
-        self.n = 0
-        self.m = 0
-        self.x = 0
-        self.diferencia = 0
-        self.errado = 0
 
     def solve(self):
-        # GaussSeidel
-        self.tamano = np.shape(self.A)
-        self.n = self.tamano[0]
-        self.m = self.tamano[1]
-        # Valores iniciales
-        self.X = np.copy(self.X0)
-        self.diferencia = np.ones(self.n, dtype=float)
-        self.errado = 2*self.tol
-
-        k = 0
-        while not (self.errado < self.tol or k > self.itermax):
-            # Por fila
-            for i in range(0, self.n, 1):
-                suma = 0
-                # Por columna
-                for j in range(0, self.m, 1):
-                    # Excluir diagonal A
-                    if (i != j):
-                        suma = suma + self.A[i, j] * self.X[j]
-
-                nuevo = (self.B[i] + suma) / self.A[i, i]
-                self.diferencia[i] = np.abs(nuevo-self.X[i])
-                self.X[i] = nuevo
-            self.errado = np.max(self.diferencia)
-            k = k + 1
-        # Respuesta de X en columna
-        self.X = np.transpose([self.X])
-        print(self.X)
-        print(k)
-        if (k > self.itermax):
-            self.X = 0
-        self.verificar = np.dot(self.A,self.X)
-        a = 1
-        b = 2
-        c = 3
-        return a , b, c
+              
+            auxa = self.a
+            auxb = self.b
+            auxc = self.c
+            if(self.op == 1):
+                self.a1 = (7.85 + 0.1*(self.b) + 0.2*(self.c)) / 3
+                self.a = self.a1
+                self.b1 = (-19.3 - 0.1*(self.a) + 0.3*(self.c)) / 7
+                self.b = self.b1
+                self.c1 = (71.4 - 0.3*(self.a) + 0.2*(self.b)) / 10
+                self.c = self.c1
+            elif(self.op == 2):
+                self.a1 = (8 + (self.b) + (self.c)) / 8
+                self.a = self.a1
+                self.b1 = (4 + 2*(self.a) - (self.c)) / 4
+                self.b = self.b1
+                self.c1 = (5 - (self.a) + 3*(self.b)) / 5
+                self.c = self.c1
+           
+            errora= self.a1 - auxa
+            errorb = self.b1 - auxb
+            errorc = self.c1 - auxc
+            while((abs(errora) > self.tol) and (abs(errorb) > self.tol) and (abs(errorc) > self.tol)):
+                auxa = self.a
+                auxb = self.b
+                auxc = self.c
+                if(self.op == 1):
+                    self.a1 = (7.85 + 0.1*(self.b) + 0.2*(self.c)) / 3
+                    self.a = self.a1
+                    self.b1 = (-19.3 - 0.1*(self.a) + 0.3*(self.c)) / 7
+                    self.b = self.b1
+                    self.c1 = (71.4 - 0.3*(self.a) + 0.2*(self.b)) / 10
+                    self.c = self.c1
+                elif(self.op == 2):
+                    self.a1 = (8 + (self.b) + (self.c)) / 8
+                    self.a = self.a1
+                    self.b1 = (4 + 2*(self.a) - (self.c)) / 4
+                    self.b = self.b1
+                    self.c1 = (5 - (self.a) + 3*(self.b)) / 5
+                    self.c = self.c1
+                errora= self.a1 - auxa
+                errorb = self.b1 - auxb
+                errorc = self.c1 - auxc
+            
+                auxa = self.a
+                auxb = self.b
+                auxc = self.c  
+            if(self.op == 1):
+                self.a1 = (7.85 + 0.1*(self.b) + 0.2*(self.c)) / 3
+                self.a = self.a1
+                self.b1 = (-19.3 - 0.1*(self.a) + 0.3*(self.c)) / 7
+                self.b = self.b1
+                self.c1 = (71.4 - 0.3*(self.a) + 0.2*(self.b)) / 10
+                self.c = self.c1
+            elif(self.op == 2):
+                self.a1 = (8 + (self.b) + (self.c)) / 8
+                self.a = self.a1
+                self.b1 = (4 + 2*(self.a) - (self.c)) / 4
+                self.b = self.b1
+                self.c1 = (5 - (self.a) + 3*(self.b)) / 5
+                self.c = self.c1
+            errora= self.a1 - auxa
+            errorb = self.b1 - auxb
+            errorc = self.c1 - auxc
+            a = self.a
+            b = self.b
+            c = self.c
+            return a,b,c
